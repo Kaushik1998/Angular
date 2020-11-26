5d6e63cc1357685c35445081b333fec1d4b2a2c2
Kaushik1998 <ksncoc@gmail.com>
2020-11-26 19:51:50 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { AppRoutingModule, routingComponents } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { NavbarComponent } from "./navbar/navbar.component";
+import { ComponentExperimentComponent } from "./component-experiment/component-experiment.component";
+import { HomepageComponent } from "./homepage/homepage.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponents,
+    NavbarComponent,
+    ComponentExperimentComponent,
+    HomepageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
